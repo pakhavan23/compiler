cfe7d0e6b7603d9ef439bab96fd36b9ccbdc2dd9
Erfan <techerfan@gmail.com>
2020-11-30 21:42:29 +0330

    Add samples to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod lexer;
+
+use lexer::SyntaxToken;
+
 fn main() {
-    println!("Hello, world!");
+  // this is erfan\n im 20\n javascript []agar {} programmer\n and \"wanna\" be a rust developer []\nsdfasd
+  let tokens: Vec<SyntaxToken> = lexer::get_tokens("Begir(\"sddf\", ali)^\nagar { 2 &MM 3 } [ ]");
+  for token in tokens {
+      println!("position: {} line: {} text: {}", token.position, token.line, token.text);
+  }
+
+  // let tokens = lexer::get_syntax("123&MM&B&BM132erfan".to_string(), 0, 1);
+
+  // for token in tokens {
+  //   println!("position: {} line: {} text: {}", token.position, token.line, token.text);
+  // }
 }
