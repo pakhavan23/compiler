2c4fb3305a329d95c75b4547b94ffe8f907fa95c
Erfan <techerfan@gmail.com>
2020-12-01 18:47:27 +0330

    Added a simple char to the sample string

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod syntax_kinds;
 use lexer::SyntaxToken;
 
 fn main() {
-  let tokens: Vec<SyntaxToken> = lexer::get_tokens("Begir(\"sddf\", ali)^\nagar { 2 &MM 3 } [ ]");
+  let tokens: Vec<SyntaxToken> = lexer::get_tokens("Begir(\"sddf\", ali)^\nagar { 2 &MM 3 } [ 'AB' 'A' ]");
   for token in tokens {
       println!("position: {} line: {} text: {}", token.position, token.line, token.text);
   }
